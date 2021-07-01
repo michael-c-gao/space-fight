cb78145e284be9d44b7e07287b9f1c42f38bb643
michael-c-gao <michaelgao16@gmail.com>
2021-06-30 20:08:58 -0700

    pause menu in tutorial, tutorial hidden when paused

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -7,24 +7,69 @@ public class TutorialManager : MonoBehaviour
 
     public GameObject[] tutorialArray;
     private int tutorialIndex = 0;
+    private static bool pressedW = false;
+    private static bool pressedA = false;
+    private static bool pressedS = false;
+    private static bool pressedD = false;
+
+
+
+
+
+    void WASDPress()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            pressedW = true;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            pressedA = true;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            pressedS = true;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            pressedD = true;
+        }
+        if (pressedW && pressedA && pressedS && pressedD)
+        {
+            tutorialIndex += 1;
+        }
+        print(tutorialIndex);
+
+    }
+
 
     void Update()
     {
-        for(int i = 0; i < tutorialArray.Length; i++)
+        for (int i = 0; i < tutorialArray.Length; i++)
         {
             if (i == tutorialIndex)
             {
                 tutorialArray[i].SetActive(true);
-            }else{
+            }
+            else
+            {
                 tutorialArray[i].SetActive(false);
-            }   
-        }   
-        if(tutorialIndex >= 0)
+            }
+            if (Pause.isPaused)
+            {
+                tutorialArray[i].SetActive(false);
+            }
+        }
+        if (tutorialIndex != 1)
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
                 tutorialIndex += 1;
             }
+        }
+        else
+        {
+            WASDPress();
         }
     }
 }

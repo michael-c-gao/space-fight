a93382e589bed92e2f329a63ee41a0e5b17a048e
michael-c-gao <michaelgao16@gmail.com>
2021-07-02 19:01:58 -0700

    added boost/spring time limit - can only sprint for 3 seconds, ability will recharge in 5 seconds. unlimited besides this.

--- Assets/Scripts/move.cs
+++ Assets/Scripts/move.cs
@@ -6,22 +6,41 @@ public class move : MonoBehaviour
 {
     public CharacterController CC;
     public float movementSpeed = 15f;
+    public float sprintSpeed = 35f;
+    public float timer = 0f;
+    public float timer2 = 0f;
+    public int timer3 = 0;
+
+
 
     void Update()
-    { 
+    {
+
         movementSpeed = 15f;
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-
+        
         Vector3 moveVector = (transform.right * horizontal) + (transform.forward * vertical);
 
 
-        if (Input.GetKey(KeyCode.LeftShift)){
-            movementSpeed = 35f;
+        if (Input.GetKey(KeyCode.LeftShift) && timer <= 3f){
+            
+            movementSpeed = sprintSpeed;
+            timer += Time.deltaTime;
         }
 
+        if (timer >= 2.9f)
+        {
+            timer2 += 1 * Time.deltaTime;
+            timer3 = Mathf.RoundToInt(timer2);
 
+            if(timer3 % 5 == 0 && timer3 != 0)
+            {
 
-        CC.Move(moveVector * movementSpeed * Time.deltaTime);
+                timer = 0; timer2 = 0; timer3 = 0;
+            }
+        }
+
+            CC.Move(moveVector * movementSpeed * Time.deltaTime);
     }
 }
--- README.md
+++ README.md
@@ -14,9 +14,9 @@ Player Models (need to learn Blender3D)
 <br/>Player Punch, Kick, Block
 <br/>Player Special Abilities
 <br/>Enemy AI
-<br/>Healthbar
+<br/>In Game UI (healthbar, special power icons/recharge bars)
 <br/>Arrow that points to location of enemy
 <br/>~~Create 3D Hollow Sphere Arena (Blender3D)~~
-<br/>Sprinting time limit (recharge bars?)
+<br/>~~Sprinting time limit~~
 <br/>Multiplayer (look into how to do this)
 <br/>Game Won Screen (at very end of project)

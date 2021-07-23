fcf1b4a266077ef272b4923666e32bfd9abff3e8
michael-c-gao <michaelgao16@gmail.com>
2021-07-23 14:26:03 -0700

    rotations

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+    void FixedUpdate()
+    {
+        transform.Rotate(0, -75 * Time.deltaTime,0);
+    }
+}

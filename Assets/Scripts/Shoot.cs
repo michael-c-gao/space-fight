965e83a91a983e2f0e598577049e5f94a29cfb9f
michael-c-gao <michaelgao16@gmail.com>
2021-08-27 18:20:27 -0700

    added sniper scope and zoom

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -17,8 +17,11 @@ public class Shoot : MonoBehaviour
     
     public GameObject impact;
     bool activeWeapon = false;
+    public GameObject sniperScope;
 
     public Image abilityBar;
+    private static bool isADS = false;
+    private static bool swappedADS = false;
     
     [SerializeField] ParticleSystem attackParticle;
 
@@ -47,22 +50,60 @@ public class Shoot : MonoBehaviour
         if (Gun.currWeapon != 1)
         {
             activeWeapon = Input.GetMouseButtonDown(0);
+            print(activeWeapon);
         }
         else
         {
             activeWeapon = Input.GetMouseButton(0);
+            
+        }
+    }
+
+    void ADS()
+    {
+
+        if(Gun.currWeapon == 2){
+            if (Input.GetMouseButtonDown(2) && !isADS) {
+                cam.fieldOfView = 30.0f;
+                sniperScope.SetActive(true);
+                isADS = true;
+                swappedADS = true;
+
+            }
+            else if (Input.GetMouseButtonDown(2) && isADS) {
+                cam.fieldOfView = 60.0f;
+                sniperScope.SetActive(false);
+                isADS = false;
+                swappedADS = false;
+            }
+        }
+        else
+        {
+            if (swappedADS)
+            {
+                //bool check so camera is not contulally set to 60 degrees over and over
+                sniperScope.SetActive(false);
+                cam.fieldOfView = 60.0f;
+                isADS = false;
+                swappedADS = false;
+            }
         }
     }
 
     void Update()
     {
         weaponCheck();
-
+        ADS();
         if (activeWeapon && (Time.time >= nextShot) && !GameOver.isGameOver && !Pause.isPaused)
         {
             nextShot = Time.time + 4 / fireRate;
             attackParticle.Play();
             shootGun();
+            if (isADS && Gun.currWeapon == 2)
+            {
+                sniperScope.SetActive(false);
+                cam.fieldOfView = 60.0f;
+            }
         }
 
         abilityBar.fillAmount = (count / maxCount);
